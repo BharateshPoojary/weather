56ae523716c6f1c8bf68bc8dde6ab0c2299f9930
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-01-27 10:51:24 +0530

    weather app started

---
+++ src/Userlocation.js
@@ -0,0 +1,8 @@
+import React from "react"
+// const api_call="https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+// import Clock from "react-live-clock" 
+// function dateaccessor() {
+//     let day=getDay();
+// console.log(day);
+// }
+// dateaccessor()
