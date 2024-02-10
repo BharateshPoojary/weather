a4797caf6be9e209c1ed680b2322fe0a427976ee
Bharatesh Poojary <bharateshpoojari@gmail.com>
2024-02-10 10:26:15 +0530

    userlocationdone

--- src/components/Userlocation.js
+++ src/components/Userlocation.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Searchlocation.css";
 import searchicon from "./search-icon.webp";
 const Userlocation = (props) => {
@@ -9,58 +9,64 @@ const Userlocation = (props) => {
   const [query, setQuery] = useState("");
   const [isvisible, setIsVisible] = useState(false);
   const apikey = "2a9d131282265146853311eea52d66c6";
-
-  navigator.geolocation.getCurrentPosition(
-    userlocationretrieved,
-    userrejectedtoaccesslocation
-  );
-  async function userlocationretrieved(position) {
-    let exampleposition = position;
-    console.log(exampleposition);
-    let user_latitude = position.coords.latitude;
-    let user_longitude = position.coords.longitude;
-    console.log(user_latitude);
-    console.log(user_longitude);
-    const accessing_weather = `https://api.openweathermap.org/data/2.5/weather?lat=${user_latitude}&lon=${user_longitude}&units=metric&appid=${apikey}`;
-    let accessing_weather_fetchingurl = await fetch(accessing_weather);
-    let accessing_weather_parseddata =
-      await accessing_weather_fetchingurl.json();
-    console.log(accessing_weather_parseddata);
-    let accessing_weather_temperature = accessing_weather_parseddata.main.temp;
-    let roundofftemperature = Math.round(accessing_weather_temperature);
-    setTemperature(roundofftemperature);
-    const converting_coordinates_to_cityname = `http://api.openweathermap.org/geo/1.0/reverse?lat=${user_latitude}&lon=${user_longitude}&appid=${apikey}`;
-    let converting_coordinates_to_cityname_fetching_url = await fetch(
-      converting_coordinates_to_cityname
+  function locationasking() {
+    setIsVisible(false)
+    navigator.geolocation.getCurrentPosition(
+      userlocationretrieved,
+      userrejectedtoaccesslocation
     );
-    let converting_coordinates_to_cityname_parseddata =
-      await converting_coordinates_to_cityname_fetching_url.json();
-    let converting_coordinates_to_cityname_data =
-      converting_coordinates_to_cityname_parseddata[0].name;
-    console.log(converting_coordinates_to_cityname_parseddata);
-    setCityName(converting_coordinates_to_cityname_data);
+    async function userlocationretrieved(position) {
+
+      let exampleposition = position;
+      console.log(exampleposition);
+      let user_latitude = position.coords.latitude;
+      let user_longitude = position.coords.longitude;
+      console.log(user_latitude);
+      console.log(user_longitude);
+      const accessing_weather = `https://api.openweathermap.org/data/2.5/weather?lat=${user_latitude}&lon=${user_longitude}&units=metric&appid=${apikey}`;
+      let accessing_weather_fetchingurl = await fetch(accessing_weather);
+      let accessing_weather_parseddata =
+        await accessing_weather_fetchingurl.json();
+      console.log(accessing_weather_parseddata);
+      let accessing_weather_temperature = accessing_weather_parseddata.main.temp;
+      let roundofftemperature = Math.round(accessing_weather_temperature);
+      setTemperature(roundofftemperature);
+      const converting_coordinates_to_cityname = `http://api.openweathermap.org/geo/1.0/reverse?lat=${user_latitude}&lon=${user_longitude}&appid=${apikey}`;
+      let converting_coordinates_to_cityname_fetching_url = await fetch(
+        converting_coordinates_to_cityname
+      );
+      let converting_coordinates_to_cityname_parseddata =
+        await converting_coordinates_to_cityname_fetching_url.json();
+      let converting_coordinates_to_cityname_data =
+        converting_coordinates_to_cityname_parseddata[0].name;
+      console.log(converting_coordinates_to_cityname_parseddata);
+      setCityName(converting_coordinates_to_cityname_data);
+    }
+
+    async function userrejectedtoaccesslocation() {
+
+      const accessing_weather = `https://api.openweathermap.org/data/2.5/weather?lat=${props.latitude}&lon=${props.longitude}&units=metric&appid=${apikey}`;
+      let accessing_weather_fetchingurl = await fetch(accessing_weather);
+      let accessing_weather_parseddata =
+        await accessing_weather_fetchingurl.json();
+      console.log(accessing_weather_parseddata);
+      let accessing_weather_temperature = accessing_weather_parseddata.main.temp;
+      let roundofftemperature = Math.round(accessing_weather_temperature);
+      setTemperature(roundofftemperature);
+      const converting_coordinates_to_cityname = `http://api.openweathermap.org/geo/1.0/reverse?lat=${props.latitude}&lon=${props.longitude}&appid=${apikey}`;
+      let converting_coordinates_to_cityname_fetching_url = await fetch(
+        converting_coordinates_to_cityname
+      );
+      let converting_coordinates_to_cityname_parseddata =
+        await converting_coordinates_to_cityname_fetching_url.json();
+      let converting_coordinates_to_cityname_data =
+        converting_coordinates_to_cityname_parseddata[0].name;
+      console.log(converting_coordinates_to_cityname_parseddata);
+      setCityName(converting_coordinates_to_cityname_data);
+    }
   }
 
-  async function userrejectedtoaccesslocation() {
-    const accessing_weather = `https://api.openweathermap.org/data/2.5/weather?lat=${props.latitude}&lon=${props.longitude}&units=metric&appid=${apikey}`;
-    let accessing_weather_fetchingurl = await fetch(accessing_weather);
-    let accessing_weather_parseddata =
-      await accessing_weather_fetchingurl.json();
-    console.log(accessing_weather_parseddata);
-    let accessing_weather_temperature = accessing_weather_parseddata.main.temp;
-    let roundofftemperature = Math.round(accessing_weather_temperature);
-    setTemperature(roundofftemperature);
-    const converting_coordinates_to_cityname = `http://api.openweathermap.org/geo/1.0/reverse?lat=${props.latitude}&lon=${props.longitude}&appid=${apikey}`;
-    let converting_coordinates_to_cityname_fetching_url = await fetch(
-      converting_coordinates_to_cityname
-    );
-    let converting_coordinates_to_cityname_parseddata =
-      await converting_coordinates_to_cityname_fetching_url.json();
-    let converting_coordinates_to_cityname_data =
-      converting_coordinates_to_cityname_parseddata[0].name;
-    console.log(converting_coordinates_to_cityname_parseddata);
-    setCityName(converting_coordinates_to_cityname_data);
-  }
+
 
 
   const searchforlocation = async () => {
@@ -104,7 +110,7 @@ const Userlocation = (props) => {
 
   return (
     <>
-      <div className="search-container">
+      <div className="search-container" onLoad={locationasking}>
         <div className="search-bar">
           <input
             type="search"
@@ -125,6 +131,8 @@ const Userlocation = (props) => {
             onClick={searchforlocation}
           />
         </div>
+        <button className="currentlocationbtn" onClick={locationasking}></button>
+
       </div>
       {isvisible || (
         <>
